fdd144075d6747e1c0490f7fbb0b406b9802d933
FrancisUzoigwe <kossyuzoigwe@gmail.com>
2024-03-05 18:29:11 +0100

    3rd commit

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,9 +1,14 @@
-import map from "../../assets/world_map.jpg"
+import map from "../../assets/worldmap.png"
 const Register = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center" style={{ background: `url(${map})`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "right" }}>
-      <div>
-        <div className="w-[400px] h-[500px] bg-[#40196D] rounded-lg"></div>
+    <div className="w-full h-screen flex justify-center items-center" style={{ background: `url(${map})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "right" }}>
+      <div className="w-full flex justify-center items-center">
+        <div className="w-[400px] max-sm:w-[80%] h-[500px] flex flex-col items-center  bg-gray-200 rounded-lg">
+          <div className="w-[90%] ">
+            <div className="mt-5 font-black text-2xl">Signup to Chyme</div>
+            <div className="my-1 text-[14px]">To signup, please add an email address and a password to get started.</div>
+          </div>
+        </div>
       </div>
     </div>
   )
