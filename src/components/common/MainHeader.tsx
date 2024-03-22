9fe523308d78cb609ee38413fc7f4991d806aacf
FrancisUzoigwe <kossyuzoigwe@gmail.com>
2024-03-22 12:10:10 +0100

    Navigation set up already

--- src/components/common/MainHeader.tsx
+++ src/components/common/MainHeader.tsx
@@ -5,6 +5,7 @@ import { IoPieChartOutline } from "react-icons/io5"
 import { CiCreditCard2, CiReceipt } from "react-icons/ci"
 import { HiOutlineMenu } from "react-icons/hi"
 import image from "../../../public/vite.svg"
+import { Link } from "react-router-dom"
 const MainHeader = () => {
   return (
 
@@ -13,18 +14,18 @@ const MainHeader = () => {
         <header className="w-[90%] flex justify-between items-center">
           <div className="text-[#40196D] font-black flex items-center" > <div className="mr-2"><SiMastercomfig /></div>Chyme</div>
           <div className="flex items-center justify-center max-md:hidden text-[#40196D]">
-            <div className="flex items-center  mx-4"><FaHome className="mx-1 text-[20px]" />Home </div>
-            <div className="flex items-center mx-4"><BsSend className="mx-1 text-[20px]" />Send </div>
-            <div className="flex items-center mx-4"><CiReceipt className="mx-1 text-[20px]" />Pay </div>
-            <div className="flex items-center mx-4"><IoPieChartOutline className="mx-1 text-[20px]" />Budget </div>
-            <div className="flex items-center mx-4"><CiCreditCard2 className="mx-1 text-[20px]" />Card </div>
+            <Link to="/auth"><div className="flex items-center  mx-4"><FaHome className="mx-1 text-[20px]" />Home </div></Link>
+            <Link to="/auth/send"><div className="flex items-center mx-4"><BsSend className="mx-1 text-[20px]" />Send </div></Link>
+            <Link to="/auth/pay">  <div className="flex items-center mx-4"><CiReceipt className="mx-1 text-[20px]" />Pay </div></Link>
+            <Link to="/auth/budget"> <div className="flex items-center mx-4"><IoPieChartOutline className="mx-1 text-[20px]" />Budget </div></Link>
+            <Link to="/auth/card">  <div className="flex items-center mx-4"><CiCreditCard2 className="mx-1 text-[20px]" />Card </div></Link>
 
           </div>
-          <div className="flex  items-center justify-center">
+          <Link to="/auth/account">  <div className="flex  items-center justify-center">
             <img alt="Img" src={image} className="w-[35px] h-[35px] rounded-full mr-1 border " />
             <div className="max-md:text-[14px]">Account</div>
             <div className="hidden max-md:block ml-3 text-2xl hover:cursor-pointer"><HiOutlineMenu /></div>
-          </div>
+          </div></Link>
         </header>
       </header>
     </header>
--- src/router/mainRoute.tsx
+++ src/router/mainRoute.tsx
@@ -8,6 +8,11 @@ import PrivateRoute from "./PrivateRoute"
 import EmailScreen from "../pages/auth/EmailScreen"
 import LoadingPage from "../pages/auth/LoadingPage"
 import DashBoard from "../pages/home/DashBoard"
+import Send from "../pages/home/Send"
+import Pay from "../pages/home/Pay"
+import Account from "../pages/home/Account"
+import Budget from "../pages/home/Budget"
+import Card from "../pages/home/Card"
 
 export const mainRoute = createBrowserRouter([
     {
@@ -47,7 +52,27 @@ export const mainRoute = createBrowserRouter([
             {
                 index: true,
                 element: <DashBoard />
-            }
+            },
+            {
+                path: "/auth/send",
+                element: <Send/>
+            },
+            {
+                path: "/auth/pay",
+                element: <Pay/>
+            },
+            {
+                path: "/auth/account",
+                element: <Account/>
+            },
+            {
+                path: "/auth/budget",
+                element: <Budget/>
+            },
+            {
+                path: "/auth/card",
+                element: <Card/>
+            },
         ]
     }
 ])
