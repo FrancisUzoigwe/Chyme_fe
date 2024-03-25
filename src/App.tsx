625e77a12efc427814a1f1c5416c76f83eb89016
Dc Valerian <Dc-Valerian>
2024-03-25 15:02:46 +0100

    added the online feature and backtotopbutton

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import { RouterProvider } from "react-router-dom"
-import { mainRoute } from "./router/mainRoute"
-import { Provider } from "react-redux"
-import { store } from "./global/store"
-import { PersistGate } from "redux-persist/integration/react"
-import { persistStore } from "redux-persist"
+import { RouterProvider } from "react-router-dom";
+import { mainRoute } from "./router/mainRoute";
+import { Provider } from "react-redux";
+import { store } from "./global/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import Online from "./components/common/OnlineFeature/Online";
+import BackToTopButton from "./components/common/BackToTopButton/BackToTopButton";
 
 const App = () => {
-  const persist = persistStore(store)
+  const persist = persistStore(store);
   return (
     <div>
       <PersistGate persistor={persist}>
         <Provider store={store}>
           <RouterProvider router={mainRoute} />
+          <Online />
+          <BackToTopButton />
         </Provider>
       </PersistGate>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
